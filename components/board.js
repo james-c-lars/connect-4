b378d10c948fcd21a967a2431904b97002c98816
James Larsen <jlarsen@ucsd.edu>
2021-02-25 12:12:42 -0800

    Added basic computer play

--- components/board.js
+++ components/board.js
@@ -1,6 +1,40 @@
+var colorZero = 'black';
+var colorOne = 'red';
+var colorTwo = 'blue';
+
 function getBoard() {
     return document.querySelector('game-board');
 }
+
+
+
+class Piece {
+    constructor() {
+        this.element = document.createElement('span');
+        this.element.setAttribute('class', 'circle');
+        this.element.style.backgroundColor = colorZero;
+    }
+
+    color() {
+        return this.element.style.backgroundColor;
+    }
+
+    fill(colorNum) {
+        this.element.style.backgroundColor = colorNum == 1? colorOne : colorTwo;
+    }
+    empty() {
+        this.element.style.backgroundColor = colorZero;
+    }
+
+    show() {
+        this.element.style.opacity = '1.0';
+    }
+    hide() {
+        this.element.style.opacity = '0.0';
+    }
+}
+
+
 
 class Board extends HTMLElement {
     constructor() {
@@ -11,14 +45,21 @@ class Board extends HTMLElement {
         let boardContent = boardTemplate.content.cloneNode(true);
         let boardDrop = boardContent.getElementById('indicator-container');
         let boardContainer = boardContent.getElementById('pieces-container');
+        this.beginButton = boardContent.getElementById('begin-button')
+        this.playerOneSelect = boardContent.getElementById('player-one');
+        this.playerTwoSelect = boardContent.getElementById('player-two');
 
-        // Add the game-pieces
+        /*
+         * Add the game pieces to the board
+         */
         this.pieces = [];
         for(let i=0; i < 7; i++) {
             this.pieces.push([]);
+
+            // Create column div element
             let column = document.createElement('div');
             column.setAttribute('class', 'column');
-            column.setAttribute('onclick', 'getBoard().dropPiece(' + i + '); getBoard().showMove(' + i + ')')
+            column.setAttribute('onclick', 'getBoard().makeMove(' + i + ')')
             column.setAttribute('onmouseover', 'getBoard().showMove(' + i + ')');
 
             for(let j=0; j < 6; j++) {
@@ -30,6 +71,9 @@ class Board extends HTMLElement {
             boardContainer.appendChild(column);
         }
 
+        /*
+         * Add the indicator element at the top of the board
+         */
         this.pieceIndicators = [];
         for(let i=0; i < 7; i++) {
             let piece = new Piece();
@@ -44,31 +88,104 @@ class Board extends HTMLElement {
             boardDrop.appendChild(column);
         }
 
-        // Set whose turn it is
-        this.turn = 'red';
+        // Whose turn it is
+        this.turn = 1;
+
+        // Whether the board is interactable
+        // Will become interactable when the move controller says it's the human's turn
+        this.enable = false;
+
+        // Attach a DecisionTree to the board
+        this.computer = new DecisionTree(this);
+
+        // Whether the AI or a human is controlling each color
+        this.control = {1:this.playerOneSelect.value, 2:this.playerTwoSelect.value};
 
         // Shadow root
         const shadowRoot = this.attachShadow({mode: 'open'});
         shadowRoot.appendChild(boardContent);
     }
 
+    beginGame() {
+        this.moveController();
+
+        this.beginButton.disabled = true;
+        this.playerOneSelect.disabled = true;
+        this.playerTwoSelect.disabled = true;
+    }
+
+    changePlayers(playerNum) {
+        if(playerNum == 1) {
+            this.control[1] = this.playerOneSelect.value;
+        } else {
+            this.control[2] = this.playerTwoSelect.value;
+        }
+    }
+
+    moveController() {
+        // If a computer is going next, make its move
+        if(this.control[this.turn] == 'computer') {
+            this.dropPiece(this.computer.bestMove());
+        }
+
+        // If the game is over, disable the board
+        if(this.computer.gameOver() != 0) {
+            this.enable = false;
+        }
+        // If the game isn't over and it's the computer's turn, take it from the top
+        else if(this.control[this.turn] == 'computer') {
+            this.moveController();
+        }
+        // If the game isn't over and it's the human's turn, let them interact with the board
+        else {
+            this.enable = true;
+        }
+    }
+
+    makeMove(column) {
+        // If the board isn't enabled don't do anything
+        if(!(this.enable)) {
+            return;
+        }
+
+        // Avoid any issues with users clicking super fast
+        this.enable = false;
+
+        // Place the piece
+        this.dropPiece(column);
+
+        // Update the indicator
+        this.showMove(column);
+
+        // Let the move controller decide what happens next
+        this.moveController();
+    }
+
     dropPiece(column) {
-        if(this.pieces[column][5].color() != 'black') {
+        /*
+         * Handling the updating of the visual board state
+         */
+
+        // If the column doesn't have any empty space return false
+        if(this.pieces[column][5].color() != colorZero) {
             return false;
         }
 
+        // Find the last open space in the column
         let row = 5;
-        while(row > 0 && this.pieces[column][row-1].color() == 'black') {
+        while(row > 0 && this.pieces[column][row-1].color() == colorZero) {
             row -= 1;
         }
 
-        if(this.turn == 'red') {
-            this.pieces[column][row].fillRed();
-            this.turn = 'blue';
-        } else {
-            this.pieces[column][row].fillBlue();
-            this.turn = 'red';
-        }
+        // Edit the piece in the board
+        this.pieces[column][row].fill(this.turn);
+        this.turn = 3 - this.turn;
+
+        /*
+         * Handling the computer's internal representation of the board state
+         */
+
+        this.computer.update(column);
 
         return true;
     }
@@ -83,7 +200,7 @@ class Board extends HTMLElement {
 
     showMove(column) {
         this.pieceIndicators[column].show();
-        this.pieceIndicators[column].element.style.backgroundColor = this.turn;
+        this.pieceIndicators[column].fill(this.turn);
 
         for(let i=0; i < 7; i++) {
             if(i != column) {
@@ -99,32 +216,3 @@ class Board extends HTMLElement {
     }
 
 } customElements.define('game-board', Board);
-
-class Piece {
-    constructor() {
-        this.element = document.createElement('span');
-        this.element.setAttribute('class', 'circle');
-        this.element.style.backgroundColor = 'black';
-    }
-
-    color() {
-        return this.element.style.backgroundColor;
-    }
-
-    fillRed() {
-        this.element.style.backgroundColor = 'red';
-    }
-    fillBlue() {
-        this.element.style.backgroundColor = 'blue';
-    }
-    empty() {
-        this.element.style.backgroundColor = 'black';
-    }
-
-    show() {
-        this.element.style.opacity = '1.0';
-    }
-    hide() {
-        this.element.style.opacity = '0.0';
-    }
-}
---
+++ scripts/computer.js
@@ -0,0 +1,395 @@
+var foresight = 10000;
+
+/**
+ * Manages the tree of BoardNodes
+ */
+class DecisionTree {
+
+    /**
+     * Constructs a DecisionTree from a Board element.
+     * @param {Board} board 
+     */
+    constructor(board) {
+        this.root = new BoardNode();
+        this.turn = board.turn;
+
+        // Constructing the board state of our root node
+        for(let col=0; col < 7; col++) {
+            for(let row=0; row < 6; row++) {
+                if(board.pieces[col][row] == colorOne) {
+                    this.root.ghostBoard[col][row] = 1;
+                } else if(board.pieces[col][row] == colorTwo) {
+                    this.root.ghostBoard[col][row] = 2;
+                } else {
+                    this.root.ghostBoard[col][row] = 0;
+                }
+            }
+        }
+
+        this.generateNodes(foresight);
+    }
+
+    size(node) {
+        let s = 1;
+        for(let i=0; i < node.children.length; i++) {
+            s += this.size(node.children[i]);
+        }
+        return s;
+    }
+
+    /**
+     * Returns 0 if no one has won the game, or the number corresponding to who won
+     * 
+     * @returns {Number}
+     */
+    gameOver() {
+        if(this.root.oneScore == Number.MAX_SAFE_INTEGER) {
+            return 1;
+        }
+        if(this.root.twoScore == Number.MAX_SAFE_INTEGER) {
+            return 2;
+        }
+
+        return 0;
+    }
+
+    /**
+     * Updates the current board state based on the column in which a piece was dropped
+     * 
+     * @param {Number} column 
+     */
+    update(column) {
+        let newRoot = null;
+        for(let i=0; i < this.root.children.length; i++) {
+            if(this.root.children[i].lastMove == column) {
+                newRoot = this.root.children[i];
+                break;
+            }
+        }
+        this.root = newRoot;
+
+        this.generateNodes(foresight);
+        console.log('Tree size: ' + this.size(this.root));
+    }
+
+    /**
+     * Returns what the best move is in the given position
+     * 
+     * @returns {Number}
+     */
+    bestMove() {
+        let bestMove = -1;
+        let maxValue = Number.MIN_SAFE_INTEGER;
+
+        for(let i=0; i < this.root.children.length; i++) {
+            let value = this.moveValue(this.root.children[i]);
+            if(value >= maxValue) {
+                bestMove = this.root.children[i].lastMove;
+                maxValue = value;
+            }
+        }
+
+        return bestMove;
+    }
+
+    /**
+     * Finds what the value of a given board state is
+     * 
+     * @param {BoardNode} node 
+     * @returns {Number}
+     */
+    moveValue(node) {
+        // Base case, return the value of the board for the last person who moved
+        if(node.children.length == 0) {
+            return node.turn == 2 ? node.oneScore - node.twoScore : node.twoScore - node.oneScore;
+        }
+
+        // Otherwise find the scores of the children
+        let childScores = []
+        for(let i=0; i < node.children.length; i++) {
+            childScores.push(this.moveValue(node.children[i]));
+        }
+
+        // Find the best of the scores
+        let max = Math.max.apply(this, childScores);
+
+        // And return it's negative (as the parent will be considering from the opposite perspective)
+        return -max;
+    }
+
+    /**
+     * Expands the tree count number of times
+     * 
+     * @param {Number} count 
+     */
+    generateNodes(count) {
+        // We will iterate through this queue and add more nodes to the end
+        let queue = this.getLeaves(this.root);
+        
+        for(let i=0; i < count && i < queue.length; i++) {
+            // If the node is a won board state
+
+            // Generate some new board states
+            let newNodes = queue[i].generateChildren();
+
+            // Append the new board states to the queue
+            for(let j=0; j < newNodes.length; j++) {
+                queue.push(newNodes[j]);
+            }
+        }
+    }
+
+    /**
+     * This returns the leaves under node
+     * 
+     * @param {BoardNode} node
+     * @returns {Array<BoardNode}
+     */
+    getLeaves(node) {
+        // Base case if this node is a leaf
+        if(node.children.length == 0) {
+            return [node];
+        }
+
+        // Otherwise recursively call this function and get the leaves in one list
+        let leaves = [];
+        for(let i=0; i < node.children.length; i++) {
+            let subLeaves = this.getLeaves(node.children[i]);
+            for(let j=0; j < subLeaves.length; j++) {
+                leaves.push(subLeaves[j]);
+            }
+        }
+
+        return leaves;
+    }
+}
+
+
+
+/**
+ * A BoardNode contains a single possible board state. You can simulate a move with a BoardNode and see the resulting evaluation.
+ */
+class BoardNode {
+    
+    /**
+     * Constructs an empty BoardNode, If passed a preexisting BoardNode, will become a copy of it.
+     * 
+     * @param {BoardNode} copy 
+     */
+    constructor(copy=null) {
+        if(copy === null) {
+            this.ghostBoard = [];
+            for(let i=0; i < 7; i++) {
+                this.ghostBoard.push([]);
+                for(let j=0; j < 6; j++) {
+                    this.ghostBoard[i][j] = 0;
+                }
+            }
+
+            this.turn = 1;
+            this.oneScore = 0;
+            this.twoScore = 0;
+            this.lastMove = null;
+            this.children = [];
+        } else {
+            this.ghostBoard = [];
+            for(let i=0; i < 7; i++) {
+                this.ghostBoard.push([]);
+                for(let j=0; j < 6; j++) {
+                    this.ghostBoard[i][j] = copy.ghostBoard[i][j];
+                }
+            }
+    
+            this.turn = copy.turn;
+            this.oneScore = copy.oneScore;
+            this.twoScore = copy.twoScore;
+            this.lastMove = copy.lastMove;
+            this.children = [];
+        }
+    }
+
+    /**
+     * Returns the score associated with the passed Number
+     * @param {Number} num 
+     * @returns {Number}
+     */
+    getScore(num) {
+        return num == 1? this.oneScore - this.twoScore : this.twoScore - this.oneScore;
+    }
+
+    /**
+     * Updates the internal board state using the column a piece was dropped in. Returns whether the operation was successful.
+     * 
+     * @param {Number} column 
+     * @returns {boolean}
+     */
+    dropPiece(column) {
+        // If the column doesn't have any empty space return false
+        if(this.ghostBoard[column][5] != 0) {
+            return false;
+        }
+
+        // Find the last open space in the column
+        let row = 5;
+        while(row > 0 && this.ghostBoard[column][row-1] == 0) {
+            row -= 1;
+        }
+
+        // Edit the piece in the board
+        this.ghostBoard[column][row] = this.turn;
+        this.turn = 3 - this.turn;
+
+        this.lastMove = column;
+
+        this.evaluate();
+        return true;
+    }
+
+    /**
+     * Generates the subsequent board states leading from this one, stores them, and returns them
+     * 
+     * @returns {Array<BoardNode>}
+     */
+    generateChildren() {
+        this.children = [];
+
+        if(this.oneScore == Number.MAX_SAFE_INTEGER || this.twoScore == Number.MAX_SAFE_INTEGER) {
+            return this.children;
+        }
+
+        for(let col=0; col < 7; col++) {
+            let child = new BoardNode(this);
+
+            // If the move is invalid, dropPiece will return false
+            if(child.dropPiece(col)) {
+                this.children.push(child);
+            }
+        }
+
+        return this.children;
+    }
+
+    /**
+     * Takes a count of pieces in a set of four. Updates the BoardNode's score variables and returns true if a connect four was found.
+     * 
+     * @param {Array<Number>} count 
+     * @returns {boolean}
+     */
+    score(count) {
+        // If there are no 2 pieces to interfere with a possible 1 connect four
+        if(count[1] != 0 && count[2] == 0) {
+            if(count[1] == 4) {
+                this.oneScore = Number.MAX_SAFE_INTEGER;
+                this.twoScore = 0;
+
+                // The game is over
+                return true;
+            } else {
+                this.oneScore += Math.pow(10, count[1] - 1);
+            }
+        }
+        // If there are no 1 pieces to interfere with a possible 2 connect four
+        else if(count[1] == 0 && count[2] != 0) {
+            if(count[2] == 4) {
+                this.oneScore = 0;
+                this.twoScore = Number.MAX_SAFE_INTEGER;
+
+                // The game is over
+                return true;
+            } else {
+                this.twoScore += Math.pow(10, count[2] - 1);
+            }
+        }
+
+        // The game is not over
+        return false;
+    }
+
+    /**
+     * Evaluates the board state and updates the score variables using the score function.
+     */
+    evaluate() {
+        this.oneScore = 0;
+        this.twoScore = 0;
+
+        let count;
+
+        /*
+         * First check horizontal sets of four
+         */
+        for(let row=0; row < 6; row++) {
+
+            // There are four sets of four pieces per row
+            // For each set of four, score it
+            for(let col=0; col < 4; col++) {
+                count = [0,0,0];
+
+                // Update count
+                for(let i=0; i < 4; i++) {
+                    count[this.ghostBoard[col+i][row]] += 1;
+                }
+
+                // Score the count
+                if(this.score(count)) return;
+            }
+        }
+
+        /*
+         * Then check vertical sets of four
+         */
+        for(let col=0; col < 7; col++) {
+
+            // There are three sets of four pieces per column
+            // For each subsequent set of four, score it
+            for(let row=0; row < 3; row++) {
+                count = [0,0,0];
+
+                // Update count
+                for(let i=0; i < 4; i++) {
+                    count[this.ghostBoard[col][row+i]] += 1;
+                }
+
+                // Score the count
+                if(this.score(count)) return;
+            }
+        }
+
+        /*
+         * Then check positive diagonal sets of four
+         */
+        for(let col=0; col < 4; col++) {
+
+            // For each subsequent set of four, score it
+            for(let row=0; row < 3; row++) {
+                count = [0,0,0];
+
+                // Update count
+                for(let i=0; i < 4; i++) {
+                    count[this.ghostBoard[col+i][row+i]] += 1;
+                }
+
+                // Score the count
+                if(this.score(count)) return;
+            }
+        }
+
+        /*
+         * Finally check negative diagonal sets of four
+         */
+        for(let col=0; col < 4; col++) {
+
+            // For each subsequent set of four, score it
+            for(let row=3; row < 5; row++) {
+                count = [0,0,0];
+
+                // Update count
+                for(let i=0; i < 4; i++) {
+                    count[this.ghostBoard[col+i][row-i]] += 1;
+                }
+
+                // Score the count
+                if(this.score(count)) return;
+            }
+        }
+    }
+}
